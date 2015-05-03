42226c2b5fa203d1af45c82e19a01ae3e6889a4d
Alex Lin <alex@opoo.org>
2015-05-03 20:13:29 +0800

    Update DocProcessor java source.

--- ext/src/main/java/com/opoopress/website/DocProcessor.java
+++ ext/src/main/java/com/opoopress/website/DocProcessor.java
@@ -18,26 +18,20 @@ package com.opoopress.website;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import org.apache.commons.io.FileUtils;
 import org.opoo.press.Collection;
 import org.opoo.press.Page;
 import org.opoo.press.ProcessorAdapter;
 import org.opoo.press.Site;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author Alex Lin
  */
-public class DocProcessor extends ProcessorAdapter{
+public class DocProcessor extends ProcessorAdapter {
     private static final Logger log = LoggerFactory.getLogger(DocProcessor.class);
 
 
@@ -46,34 +40,36 @@ public class DocProcessor extends ProcessorAdapter{
         Collection collection = site.getCollections().get("doc");
         List<? extends Page> pages = collection.getPages();
 
-        List<Map<String,Object>> versions = site.getConfig().get("doc_versions");
+        List<Map<String, Object>> versions = site.getConfig().get("doc_versions");
 
-        System.out.println("Processing doc versions...");
+        log.info("Processing doc versions...");
+        System.out.println("================");
         System.out.println(versions);
+        System.out.println("----------------");
 
-        if(versions == null || versions.isEmpty()){
+        if (versions == null || versions.isEmpty()) {
             throw new IllegalArgumentException("doc versions not defined in configuration.");
         }
 
-        for(Map<String,Object> version: versions){
+        for (Map<String, Object> version : versions) {
             String ver = (String) version.get("version");
 //            String path = (String) version.get("path");
             String urlPrefix = (String) version.get("url_prefix");
-            List<Map<String,Object>> items = site.getConfig().get("doc_items_" + ver);
+            List<Map<String, Object>> items = site.getConfig().get("doc_items_" + ver);
 
-            if(items == null || items.isEmpty()){
+            if (items == null || items.isEmpty()) {
                 throw new IllegalArgumentException("doc items not found: doc_items_" + ver);
             }
 
             version.put("items", items);
 
-            for(Map<String,Object> item: items){
+            for (Map<String, Object> item : items) {
 //                String title = (String) item.get("title");
                 List<String> docs = (List<String>) item.get("docs");
                 List<Page> docPages = Lists.newArrayList();
                 item.put("pages", docPages);
 
-                for(String doc: docs){
+                for (String doc : docs) {
                     final String docUrl = urlPrefix + doc;
                     Page page = Iterables.tryFind(pages, new Predicate<Page>() {
                         @Override
@@ -82,7 +78,7 @@ public class DocProcessor extends ProcessorAdapter{
                         }
                     }).orNull();
 
-                    if(page == null){
+                    if (page == null) {
                         throw new IllegalStateException("doc url not found: " + docUrl);
                     }
                     docPages.add(page);
@@ -91,24 +87,5 @@ public class DocProcessor extends ProcessorAdapter{
                 }
             }
         }
-
-        System.out.println(versions);
     }
-
-
-    public static void main(String[] args) throws IOException {
-        File file = new File("config-doc.yml");
-        Map map1 = new Yaml().loadAs(FileUtils.openInputStream(file), Map.class);
-        System.out.println(map1);
-
-
-        List<String> stringList = Lists.newArrayList();
-        stringList.add("/overview/");
-        stringList.add("/quickstart/");
-
-        HashMap<Object, Object> map = Maps.newHashMap();
-        map.put("docs", stringList);
-        System.out.println(new Yaml().dumpAsMap(map));
-    }
-
 }
